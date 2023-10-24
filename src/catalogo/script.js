414309bc50a7fe36b2f5904eb2ee1f2bcd2027c9
axelcastrov <axelcastro117@gmail.com>
2023-10-24 07:24:37 -0700

    ej 6:Conectamos API

--- src/catalogo/script.js
+++ src/catalogo/script.js
@@ -1,133 +1,132 @@
-//#region Modelos
-
-class VideoGame {
-
-    constructor(id, name, description, category, price, image) {
-      this.id = id;
-      this.name = name;
-      this.description = description;
-      this.category = category;
-      this.price = price;
-      this.image = image;
-    }
-  
+class Videojuego {
+  constructor(id, image, nombre, descripcion, categoria, precio) {
+    this.id = id;
+    this.image = image;
+    this.nombre = nombre;
+    this.descripcion = descripcion;
+    this.categoria = categoria;
+    this.precio = precio;
   }
-  
-const videogame1 = 
-    new VideoGame(
-        1,
-        "The Witcher 3: Wild Hunt", 
-        "Un juego de rol de acción con mundo abierto que sigue las aventuras del cazador de monstruos Geralt de Rivia.", 
-        "RPG de Accion",
-        29.99, 
-        "witcher3.jpeg"
-        );
-  
-const videogame2 = 
-    new VideoGame(
-        2, 
-        "The Legend of Zelda: Breath of the Wild", 
-        "Un juego de acción y aventura de mundo abierto que sigue las aventuras de Link en un vasto y hermoso reino de Hyrule.",
-        "Aventura", 
-        59.99, 
-        "zelda.jpeg"
-        );
+}
 
-const videogame3 = 
-    new VideoGame(
-        3, 
-        "Overwatch", 
-        "Un shooter en primera persona donde los jugadores forman equipos y luchan en intensas batallas en línea para alcanzar objetivos específicos.", 
-        "Shooter",
-        39.99, 
-        "overwatch.jpeg"
-        );
-      
-const videogamesList = [videogame1, videogame2, videogame3];
-  
-  //#endregion
+// Arreglo de videojuegos
+let VideojuegoList = [];
 
-//#region despliegue tabla
-
+// Función para mostrar la tabla de videojuegos
 function displayTable(games) {
+  clearTable();
+  showLoadingMessage();
 
-    clearTable();
-
+  setTimeout(() => {
+    hideMessage();
     const tablaBody = document.getElementById('data-table-body');
-
-    const imagePath = `../assets/img/catalogo/`;
+    const imagePath = '../assets/img/catalogo/';
 
     games.forEach(game => {
-
       const row = document.createElement('tr');
-
       row.innerHTML = `
-        <td> ${game.id} </td>
-        <td> <img src="${imagePath + game.image}" alt="${game.name}" width="100"> </td>
-        <td>${game.name}</td>
-        <td>${game.description}</td>
-        <td>${game.category}</td>
-        <td>${game.price}</td>
+        <td>${game.id}</td>
+        <td><img src="${imagePath + game.image}" alt="${game.nombre}" width="100"></td>
+        <td>${game.nombre}</td>
+        <td>${game.descripcion}</td>
+        <td>${game.categoria}</td>
+        <td>${game.precio}</td>
       `;
 
       tablaBody.appendChild(row);
-
     });
+  }, 2000); // Retraso de 2 segundos (2000 ms) antes de mostrar los datos
 }
 
-// #endregion
-
+// Función para limpiar la tabla
 function clearTable() {
-    const tableBody = document.getElementById('data-table-body');
-  
-    tableBody.innerHTML = '';
-  }
+  const tableBody = document.getElementById('data-table-body');
+  tableBody.innerHTML = '';
+}
 
-//#region Filtrado
+// Función para mostrar el mensaje de carga
+function showLoadingMessage() {
+  const messageNotFound = document.getElementById('message-not-found');
+  messageNotFound.innerHTML = 'Cargando...';
+  messageNotFound.style.display = 'block';
+}
 
-// inicializar eventos de los botones del filtro
-function initButtonsHandler() {
+// Función para ocultar el mensaje de carga
+function hideMessage() {
+  const messageNotFound = document.getElementById('message-not-found');
+  messageNotFound.style.display = 'none';
+}
 
-    document.getElementById('filter-form').addEventListener('submit', event => {
-      event.preventDefault();
-      applyFilters();
-    });
-  
-    document.getElementById('reset-filters').addEventListener('click', () => {
-      document.querySelectorAll('input.filter-field').forEach(input => input.value = '');
-      applyFilters();
-    });
-  
-  }
-  
-  
-  // aplicacion del filtro a los datos y despliegue
-  function applyFilters() {
-    const filterText = document.getElementById('text').value.toLowerCase();
-    const filterCategory = document.getElementById('category').value.toLowerCase();
-    const filterMinPrice = parseFloat(document.getElementById('price-min').value);
-    const filterMaxPrice = parseFloat(document.getElementById('price-max').value);
-  
-    const filteredGames = filterGames(videogamesList, filterText, filterCategory, filterMinPrice, filterMaxPrice);
-  
-    displayTable(filteredGames);
-  }
-  
-  
-  // logica para filtrar
-  function filterGames(games, text, category, minPrice, maxPrice) {
-  
-    return games.filter( game =>
-        (!text     || game.name.toLowerCase().includes(text) || game.description.toLowerCase().includes(text)) &&
-        (!category || game.category.toLowerCase().includes(category)) &&
-        (!minPrice || game.price >= minPrice) &&
-        (!maxPrice || game.price <= maxPrice)
-      );
-  }
-  
-  //#endregion
+// Función para obtener los datos de la API
+function searchData() {
+  const OPTIONS = {
+    method: 'GET',
+    headers: {
+      'Accept': 'application json'
+    }
+  };
 
-displayTable(videogamesList);
-initButtonsHandler();
+  fetch('https://6536a0c4bb226bb85dd26f80.mockapi.io/juegos', OPTIONS)
+    .then(response => response.json())
+    .then(data => {
+      VideojuegoList = data.map(item => {
+        const videojuego = new Videojuego(
+          item.id,
+          item.image,
+          item.name,
+          item.description,
+          item.categoria,
+          item.price
+        );
+        // Agregar el nombre al select
+        const select = document.querySelector('#text');
+        const option = document.createElement('option');
+        option.value = videojuego.nombre;
+        option.text = videojuego.nombre;
+        select.appendChild(option);
 
-  
+        return videojuego;
+      });
+      displayTable(VideojuegoList);
+    })
+    .catch(error => console.log(error));
+}
+
+searchData();
+
+// Obtener los elementos del formulario
+const form = document.querySelector('#filter-form');
+const textInput = document.querySelector('#text');
+const categoryInput = document.querySelector('#category');
+const minPriceInput = document.querySelector('#price-min');
+const maxPriceInput = document.querySelector('#price-max');
+
+// Manejar el evento de envío del formulario
+form.addEventListener('submit', function (e) {
+  e.preventDefault(); // Evita que se recargue la página al enviar el formulario
+
+  // Obtener los valores de entrada del usuario
+  const filterText = textInput.value.toLowerCase();
+  const filterCategory = categoryInput.value.toLowerCase();
+  const filterMinPrice = parseFloat(minPriceInput.value);
+  const filterMaxPrice = parseFloat(maxPriceInput.value);
+
+  // Filtrar los videojuegos basados en los criterios
+  const videojuegosFiltrados = VideojuegoList.filter(function (videojuego) {
+    const cumpleTexto = videojuego.nombre.toLowerCase().includes(filterText);
+    const cumpleCategoria = videojuego.categoria.toLowerCase().includes(filterCategory);
+    const cumplePrecioMin = isNaN(filterMinPrice) || videojuego.precio >= filterMinPrice;
+    const cumplePrecioMax = isNaN(filterMaxPrice) || videojuego.precio <= filterMaxPrice;
+
+    return cumpleTexto && cumpleCategoria && cumplePrecioMin && cumplePrecioMax;
+  });
+
+  // Mostrar los videojuegos filtrados en la tabla
+  displayTable(videojuegosFiltrados);
+});
+
+// Manejar el evento de restablecimiento del formulario
+document.querySelector('#reset-filters').addEventListener('click', function () {
+  form.reset(); // Restablecer el formulario
+  displayTable(VideojuegoList); // Mostrar todos los videojuegos nuevamente
+});
